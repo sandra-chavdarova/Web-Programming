4045335f72e4c485b684005456763ef0ed872cca
Sandra Chavdarova <148618364+sandra-chavdarova@users.noreply.github.com>
2025-12-26 19:24:56 +0100

    Update

--- Exercises/Archaeological Sites/src/test/java/mk/ukim/finki/wp/jan2025g1/SeleniumScenarioTest.java
+++ Exercises/Archaeological Sites/src/test/java/mk/ukim/finki/wp/jan2025g1/SeleniumScenarioTest.java
@@ -37,7 +37,7 @@ public class SeleniumScenarioTest {
     static {
         SubmissionHelper.exam = "2025-jan-g1";
         //TODO: CHANGE THE VALUE OF THE SubmissionHelper.index WITH YOUR INDEX NUMBER!!!
-        SubmissionHelper.index = "231026";
+        SubmissionHelper.index = "230000";
     }
 
     @Autowired
